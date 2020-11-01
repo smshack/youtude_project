5d6ec4e28298998076d4d474ed7c2b914ad1f34b
smshack <5432tat@naver.com>
2020-11-01 21:49:55 +0900

    mostpopular/search Refactoring

--- src/app.jsx
+++ src/app.jsx
@@ -1,45 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
-import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-  const search =query =>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDu22-vfSZO4Jja3HdmcH3CGUrmcvn8AoE`,
-       requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+  const search = query => {
+    youtube
+      .search(query) //
+      .then(videos => setVideos(videos));
   };
+
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDu22-vfSZO4Jja3HdmcH3CGUrmcvn8AoE", requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item =>({...item,id:item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
-
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
   }, []);
-
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />;
+      <VideoList videos={videos} />
     </div>
   );
-
-
 }
 
 export default App;
